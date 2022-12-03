0850e4236901a313ab9eb291fda10c739b17da2a
DHolets99 <dholets99@gmail.com>
2022-12-03 18:44:24 +0300

    feat: realize func getColumnHeight feat: realize func getWeekDay feat: realize func addTodayClass feat: realize class column__thumb_active

--- js/index.js
+++ js/index.js
@@ -1,15 +1,38 @@
-const ul = document.getElementById('aut');
+const days = document.querySelectorAll('.column__day');
+const columns = document.querySelectorAll('.column__thumb');
+const arr = [];
 
-let db = fetch('data.json');
+let date = new Date();
+const getWeekDay = () => {
+    let days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+    return days[date.getDay()];
+};
 
-console.log(db);
 (async () => {
-    let DB = await(await fetch('data.json')).json();
-
-    for (let i = 0; i < DB.length; i++) {
-        ul.append(`${DB[i].day}: ${DB[i].amount}`);
-    };
+    let obj = await (await fetch('data.json')).json();
+    obj.forEach(el => {
+        arr.push(el);
+    }); 
+    getColumnHeight();
 })();
+
+const getColumnHeight = () => {
+    arr.forEach(el => {
+        let [day, amount] = [Object.values(el)[0], Object.values(el)[1]];
+        for (i = 0; i < days.length; i++) {
+            if (day == days[i].id) {
+                columns[i].style.height = 2 * `${amount}` + 'px';
+            };
+            addToday(days[i].id);
+        };
+    });
+};
+
+const addToday = (day) => {
+    if (getWeekDay(date) == day) {
+        columns[i].classList.toggle('column__thumb_active');
+    }
+};
 
 
 
